a0ef0295eebc04d1c8e54fbb85b033e6e12efa8c
Jimmy Claw <jimmy@claw.dev>
2026-03-20 19:22:31 +0000

    chore: add .gitignore, clean up build artifacts from repo

--- build-ui/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_yolo_ui_FILE /home/jimmy/logos-yolo-module/build-ui/yolo.so)
-set(__QT_DEPLOY_TARGET_yolo_ui_TYPE SHARED_LIBRARY)
--- src/yolo_ui_component.cpp
+++ src/yolo_ui_component.cpp
@@ -1,15 +1,9 @@
 #include "yolo_ui_component.h"
 #include <QWidget>
-#include <QLabel>
-#include <QVBoxLayout>
 
 QWidget* YoloUIComponent::createWidget(LogosAPI* logosAPI) {
     Q_UNUSED(logosAPI)
-    auto* widget = new QWidget();
-    auto* layout = new QVBoxLayout(widget);
-    auto* label = new QLabel("YOLO - Your Own Local Opinion");
-    layout->addWidget(label);
-    return widget;
+    return new QWidget();
 }
 
 void YoloUIComponent::destroyWidget(QWidget* widget) {
